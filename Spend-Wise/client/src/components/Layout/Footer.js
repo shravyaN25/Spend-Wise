782488796945bad97840589289db528a61cff6d5
Shravya N <109436935+shravyaN25@users.noreply.github.com>
2025-02-14 15:41:01 +0530

    Update Footer.js

--- Spend-Wise/client/src/components/Layout/Footer.js
+++ Spend-Wise/client/src/components/Layout/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="footer">
-      <h6 className="footer-text">All rights reserved &copy; Ovi62</h6>
+      <h6 className="footer-text">All rights reserved &copy; Shravya</h6>
     </div>
   );
 };
